7f95a6c3ad27770eac43ac6d4f3d586279e6754f
ChristopherSemard <christophersemard76@gmail.com>
2022-05-17 17:15:53 +0200

    integration header/footer, fomulaires inscription/connexion

--- inscription.php
+++ inscription.php
@@ -7,26 +7,23 @@
     <?php include_once('header.php'); ?>
 
         <h2 class="text-center mb-4">INSCRIPTION</h2>
-        <form class="container">
-            <div class="form-group">
-                <label for="inputPseudo">Email address</label>
-                <input type="text" class="form-control" id="inputPseudo" aria-describedby="emailHelp" placeholder="Pseudo">
-            </div>
-            <div class="form-group">
-                <label for="inputPassword">Password</label>
-                <input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe">
-            </div>
-            <div class="form-group">
-                <label for="inputConfirmPassword">Confirmer le mot de passe</label>
-                <input type="password" class="form-control" id="inputConfirmPassword" placeholder="Mot de passe">
-            </div>
+        <form class="container  d-flex flex-column gap-3" method="POST" action="./submit-inscription.php">
+            <!-- Choix de pseudo -->
+            <label for="inputPseudo">Pseudo</label>
+            <input type="text" class="form-control" id="inputPseudo" name="pseudo" placeholder="Pseudo" required>
+            <!-- Choix de mot de passe -->
+            <label for="inputPassword">Mot de passe</label>
+            <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Mot de passe" required>
+            <!-- Confirmation mot de passe -->
+            <label for="inputConfirmPassword">Confirmer le mot de passe</label>
+            <input type="password" class="form-control" id="inputConfirmPassword" name="password-confirm" placeholder="Mot de passe" required>
+            <!-- Choix de couleur -->
+            <label for="inputColor">Couleur du pseudo</label>
+            <input type="color" id="inputColor" name="color" value="#ffdd00">
+            <!-- Bouton envoyer -->
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
 
-    </main>
 
-
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</body>
-</html>
+    <!-- Intégration du footer -->
+    <?php include_once('footer.php'); ?>
